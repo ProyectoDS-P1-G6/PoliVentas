73f91f7a9822120c07f4c9b672d32517e07ccf1b
LuisR <luishrt1997@gmail.com>
2019-01-18 00:58:24 -0500

    pantalla de inicio

--- PoliVentasApp/src/PoliVentas/PoliVentasApp.java
+++ PoliVentasApp/src/PoliVentas/PoliVentasApp.java
@@ -6,11 +6,13 @@ package PoliVentas;
  * and open the template in the editor.
  */
 
+import controllers.InicioController;
 import controllers.LoginController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import models.entities.Usuario;
 import utils.StageDecoratorX;
+import views.Inicio;
 import views.LoginView;
 
 /**
@@ -22,10 +24,10 @@ public class PoliVentasApp extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        LoginView loginView = new LoginView();
-        new StageDecoratorX(loginView); // decorator
-        new LoginController(new Usuario(), loginView);
-        loginView.show();
+        Inicio inicioView = new Inicio();
+        new StageDecoratorX(inicioView); // decorator
+        new InicioController(inicioView);
+        inicioView.show();
     }
 
     public static void main(String[] args) {
